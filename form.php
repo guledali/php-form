6633923ea82a5c1be2bbc712ec71d0088fd2133d
guledali <guled.ali01@gmail.com>
2017-02-28 23:54:28 +0100

    adding files

---
+++ form.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="form.css" type="text/css">
+    </head>
+    <body>
+
+        <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "accounts";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+$sql = "INSERT INTO users (username, email, password, avatar) VALUES ('".$_POST["username"]."','".$_POST["email"]."','".$_POST["password"]."','dsasa')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+<div>
+    Welcome <?php echo $_POST["username"]; ?><br>   
+
+</div>
+       
+<div class="body-content">
+  <div class="module">
+    <h1>Create an account</h1>
+    <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
+      <div class="alert alert-error"></div>
+      <input type="text" placeholder="User Name" name="username" required />
+      <input type="email" placeholder="Email" name="email" required />
+      <input type="password" placeholder="Password" name="password" autocomplete="new-password" required />
+      <input type="password" placeholder="Confirm Password" name="confirmpassword" autocomplete="new-password" required />
+      <div class="avatar"><label>Select your avatar: </label><input type="file" name="avatar" accept="image/*" required /></div>
+      <input type="submit" value="Register" name="register" class="btn btn-block btn-primary" />
+    </form>
+  </div>
+</div>
+    
+    </body>
+</html>
